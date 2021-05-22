8548a042f714105f5a7e5f0aacbc0afb5515a993
Noemi Honorato <noemishonorato@gmail.com>
2021-05-22 15:45:43 -0300

    Lista 03 Busca Binária - Finalizada

--- lista3/BuscaBinaria.cpp
+++ lista3/BuscaBinaria.cpp
@@ -2,31 +2,35 @@
 
 /* Busca Binaria- Busaca recursivamente a chave dentro do vetor. A cada iteração
    reduz o espaço de busca pela metade. Quando encontra a chave retorna
-   o índice(meio) onde ele foi encontrado. Caso contrário retorna -1 */
+   o índice(meio) onde ele foi encontrado. Caso contrário retorna -1 
 
+   Relação de recorrência: 
+   T(n) = 1*T(n/2)+ Θ(1)
+        = T(n/2)+1
+*/
 
 int buscaBinaria(std::vector<int> &vetor, int esquerdo, int direito, int chave)
 
 {
-    int meio = (esquerdo + direito) / 2;
+    int meio = (esquerdo + direito) / 2; //C1+C2+C3|Três constantes somadas é uma constante Θ(1)
 
-    if (esquerdo > direito)
+    if (esquerdo > direito) //Busca enquanto tiver espaso no vetor 
     {
-        return -1;
+        return -1; //Se o valor não for encontrado retorna -1
     }
 
-    if (vetor[meio] == chave)
+    if (vetor[meio] == chave) //Cálculo do valor médio
     {
-        return meio;
+        return meio; //Se for igual o índice encontrado 
     }
 
-    if (vetor[meio] < chave)
-    {
-        return buscaBinaria(vetor, meio + 1, direito, chave);
+    if (vetor[meio] < chave) // Se não, busca a metade da direita
+    { // Ou entrar aqui
+        return buscaBinaria(vetor, meio + 1, direito, chave); //reduz o vetor do meio ao fim
     }
-    else
+    else // Ou aqui... ou uma ou outra. | T(n)=1
     { // vetor[meio] > chave
-        return buscaBinaria(vetor, esquerdo, meio - 1, chave);
+        return buscaBinaria(vetor, esquerdo, meio - 1, chave); //reduz o vetor do inicio ao meio
     }
 }
 

36f955d5e93c64cc45fca6a29ff078371bd8ba0c
Noemi Honorato <noemishonorato@gmail.com>
2021-05-24 13:37:42 -0300

    Lista 03 - Busca em Profundidade - Finalizada.

---
+++ lista2/BuscaemProfundidade2.cpp
@@ -0,0 +1,85 @@
+// Algoritmo de Busca em Profundidade em C++ 
+#include <iostream>
+#include <list>
+#include <memory>
+   
+class Graph
+{
+private:
+    int _V;    // número de vértices
+    bool _direcionado;
+    std::unique_ptr< std::list<int> > adj;  // ponteiro para um array contendo as listas de adjacências
+    void BuscaUtil(int v, bool visitado[]);
+      
+public:
+    Graph(int V, bool direcionado);// construtor
+    void AddAresta(int v, int w); // adiciona uma aresta no grafo
+    void BuscaEmProfundidade(int s); //faz uma busca em profundidade a partir de um vértice
+};
+   
+Graph::Graph(int V, bool direcionado) : adj(new std::list<int>[V]) // cria as listas
+{
+    _V = V;// atribui o número de vértices
+    _direcionado = direcionado;       
+}   
+   
+void Graph::AddAresta(int v, int w)
+{
+    std::list<int>* adjacente = adj.get();
+    adjacente[v].push_back(w);// adiciona vértice v à lista de vértices adjacentes de w
+   
+    if (!_direcionado)
+    {
+        adjacente[w].push_back(v);// insere "v" na lista Adiciona um novo elemento no final do vetor
+    }
+}
+  
+void Graph::BuscaUtil(int v, bool visitado[])
+{
+    //Marque o nó atual como visitado e imprima-o
+    visitado[v] = true; // marca como visitado
+    std::cout << v << " ";
+  
+    // Pecorre para todos os vértices adjacentes a este vértice
+    std::list<int>::iterator i;
+    for (i = (adj.get())[v].begin(); i != (adj.get())[v].end(); ++i)
+        if (!visitado[*i])
+            BuscaUtil(*i, visitado);
+}
+  
+// Traversal da busca em profundidade dos vértices alcançáveis de v. 
+//Ele usa BuscaUtil() recursivo
+void Graph::BuscaEmProfundidade(int v)
+{
+    // Marque todos os vértices como não visitados
+    std::unique_ptr<bool[]> visitado(new bool[_V]);
+     
+    for (int i = 0; i < _V; i++)
+        visitado[i] = false;
+  
+    // Chame a função auxiliar recursiva 
+    //para imprimir o percurso da busca em proundidade
+    BuscaUtil(v, visitado.get());
+}
+  
+int main()
+{
+    // Crie um graph fornecido no diagrama acima
+    Graph g(7, true);
+    g.AddAresta(0, 1);
+    g.AddAresta(0, 2);
+    g.AddAresta(0, 3);
+    g.AddAresta(1, 0);
+    g.AddAresta(1, 5);
+    g.AddAresta(2, 5);
+    g.AddAresta(3, 0);
+    g.AddAresta(3, 4);
+    g.AddAresta(4, 6);
+    g.AddAresta(5, 1);
+    g.AddAresta(6, 5);
+  
+    std::cout << "Busca em profundidade iniciando com vertice 2:\n";
+    g.BuscaEmProfundidade(2);
+  
+    return 0;
+}
